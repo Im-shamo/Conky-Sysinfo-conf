2a2b71f5f4e48a648db06e1d2a7c6941d923df7d
Im-shamo <59657733+Im-shamo@users.noreply.github.com>
2023-08-18 22:38:45 +0800

    Updated

--- conky_open.sh
+++ conky_open.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #change $HOME/.config/conky to the dir of where conky_sysinfo.conf is in
-conky -dq -c "$HOME/.config/conky/conky_sysinfo.conf" & exit
+conky -dq -c "$HOME/.config/conky/Conky-Sysinfo-conf/conky_sysinfo.conf" & exit
